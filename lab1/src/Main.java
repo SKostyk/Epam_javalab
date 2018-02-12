268f3640e386a554bcdbd76f8b6a781dc7d6f19d
SKostyk <Svyatoslav.kostyk@mail.com>
2018-02-12 14:03:56 +0200

    labs

---
+++ lab1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static int a;
+    public static char b;
+    public static void main (String[]args){
+        System.out.println("int a = " + a);
+        System.out.println("int c = " + b);
+    }
+}
