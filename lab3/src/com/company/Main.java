6051966ce02d02a67fd9dbb5c7bf422cf4a0370a
SKostyk <32357182+SKostyk@users.noreply.github.com>
2018-02-13 15:47:28 +0200

    Update Main.java

--- lab3/src/com/company/Main.java
+++ lab3/src/com/company/Main.java
@@ -4,10 +4,10 @@ import com.sun.org.apache.xpath.internal.operations.Plus;
 
 public class Main {
     public static void main(String[]args){
-        New Plus = new New();
-        Plus.a = 10;
-        Plus.b = 90;
-        Plus.result = Plus.a + Plus.b;
+        New rg = new New();
+        rg.a = 10;
+        rg.b = 90;
+        rg.result = rg.a + rg.b;
         System.out.println(Plus.result);
     }
 }
